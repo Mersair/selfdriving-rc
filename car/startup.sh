078ef5cf541c5b53c14af0da0c28f9e9942d64d2
Mersair <16909606+Mersair@users.noreply.github.com>
2021-02-09 15:45:24 -0500

    Reverted directory structure to working state

--- car/startup.sh
+++ car/startup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+curl -X POST -H "Content-Type: application/json" \
+-d '{"ultrasonic_1":13.5, "temp": 88.1, "imu":[0.83, 0.12, 0.91]}' \
+https://ai-car.herokuapp.com/api/car/1/sensordata/upload
