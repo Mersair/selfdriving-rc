9b1061ea526ada7088e2a924b4852aae5c3d0d7b
Mersair <16909606+Mersair@users.noreply.github.com>
2021-03-28 16:25:25 -0400

    Changed startup script

--- car/startup.sh
+++ car/startup.sh
@@ -6,7 +6,7 @@ FLASK_DEBUG_IP="127.0.0.1:5000"
 RUNNING_PROCESSES=`lsof -i -P -n | grep $FLASK_DEBUG_IP`
 
 if [ -z $RUNNING_PROCESSES ]; then
-echo "No flask debug server found. Connecting to remote server at '$SERVER_URL'.\n"
+echo "No flask debug server found. Connecting to remote server at '$SERVER_URL'."
 else
 echo "Flask debug server detected. Connecting to '$FLASK_DEBUG_IP'."
 SERVER_URL="$FLASK_DEBUG_IP"
@@ -16,6 +16,7 @@ echo "Attempting to enroll car"
 ENROLL_RESPONSE=`curl -sX GET $SERVER_URL/api/enroll | jq -r '.id'`
 if [ ${#ENROLL_RESPONSE} -ge 36 ]; then
 echo "Enrolled succesfully with the id '$ENROLL_RESPONSE'"
+echo $ENROLL_RESPONSE > /etc/selfdriving-rc/car_id
 else
 echo "Could not enroll car. Please ensure server is online."
 exit 1
@@ -36,11 +37,10 @@ do
     -d '{"hall_effect":13.5, "battery":96, "temperature":103.12, "humidity":28, "imu":[0.83, 0.12, 0.91]}' \
     $BASEURL/api/car/$CARID/data
     echo "Checking for any start/stop commands\n"
-    CAR_IS_RUNNING_RESPONSE=`curl $BASEURL/api/car/$CARID/control`
-    if [ "$CAR_IS_RUNNING_RESPONSE" == "true" ]; then
-    echo "Car is set to driving"
-    else
-    echo "Car is not set to driving"
-    fi
+    CAR_SPEED = `echo $CONTROL_RESPONSE | jq -r '.speed'`
+    CAR_COLOR_HIGH = `echo $CONTROL_RESPONSE | jq -r '.upper_channel'`
+    CAR_COLOR_LOW = `echo $CONTROL_RESPONSE | jq -r '.lower_channel'`
+    echo $CAR_SPEED > /etc/selfdriving-rc/car_speed
+    echo "$CAR_COLOR_LOW\n$CAR_COLOR_HIGH" > /etc/selfdriving-rc/car_color
     sleep 0.5
 done
