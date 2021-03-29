6749baeb961f2c6bbf2acf3c90b8e5c69fe43d49
Mersair <16909606+Mersair@users.noreply.github.com>
2021-03-29 18:01:43 -0400

    Startup logic

--- car/startup.sh
+++ car/startup.sh
@@ -24,23 +24,35 @@ fi
 
 while :
 do
+    echo "Collecting sensor data\n"
+    SENSOR_READING=`cat /dev/ttyACM0 9600`
     echo "Sending sensor data\n"
     UPLOAD_RESPONSE=`curl -sX POST -H "Content-Type: application/json" \
-    -d '{"hall_effect":13.5, "battery":96, "temperature":103.12, "humidity":28, "imu":[0.83, 0.12, 0.91]}' \
-    $BASEURL/api/car/$CARID/data`
+    -d '{"sensor_string":$SENSOR_READING}' $BASEURL/api/car/$CARID/data`
     if [ "$UPLOAD_RESPONSE" == "200 OK" ]; then
     echo "Server ackd sensor data"
     else
     echo "Server is offline, or rejected the sensor data"
     fi
-    sleep 0.5curl -sX POST -H "Content-Type: application/json" \
-    -d '{"hall_effect":13.5, "battery":96, "temperature":103.12, "humidity":28, "imu":[0.83, 0.12, 0.91]}' \
-    $BASEURL/api/car/$CARID/data
     echo "Checking for any start/stop commands\n"
-    CAR_SPEED = `echo $CONTROL_RESPONSE | jq -r '.speed'`
-    CAR_COLOR_HIGH = `echo $CONTROL_RESPONSE | jq -r '.upper_channel'`
-    CAR_COLOR_LOW = `echo $CONTROL_RESPONSE | jq -r '.lower_channel'`
+    CAR_SPEED = `curl -sX GET $SERVER_URL/api/speed | jq -r '.speed'`
+    COLOR_RESPONSE = `curl -sX GET $SERVER_URL/api/color_channels`
+    CAR_COLOR_HIGH = `echo $COLOR_RESPONSE | jq -r '.upper_channel'`
+    CAR_COLOR_LOW = `echo $COLOR_RESPONSE | jq -r '.lower_channel'`
     echo $CAR_SPEED > /etc/selfdriving-rc/car_speed
     echo "$CAR_COLOR_LOW\n$CAR_COLOR_HIGH" > /etc/selfdriving-rc/car_color
+    if [ $CAR_SPEED -lt 0 ]; then
+    if [ $IS_DRIVING = 0 ] then
+    echo "Car speed is now greater than zero. Starting driving script."
+    IS_DRIVING = 1
+    DRIVING_SCRIPT_PID = `python3 driving.py &`
+    fi
+    else
+    if [ $IS_DRIVING -ge 1 ] then
+    echo "Car speed is now zero. Ending driving script."
+    IS_DRIVING = 0
+    kill $DRIVING_SCRIPT_PID
+    fi
+    fi
     sleep 0.5
 done
